45720e1e12797d001a2b499c3a99d9546cb11cf7
InsAbbs <132210708+InsAbbs@users.noreply.github.com>
2024-01-17 08:39:04 +0100

    Update lfb_5_ml.py

--- streamlit-app/lfb_5_ml.py
+++ streamlit-app/lfb_5_ml.py
@@ -4,9 +4,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True
-
-
 @st.cache_data(experimental_allow_widgets=True)
 def machine():
     st.title("Modélisation 🪄")
